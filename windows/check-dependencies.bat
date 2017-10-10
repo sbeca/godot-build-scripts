200b452396fb6161d43f77205c6d977c9150b44d
Scott Beca <sbeca@users.noreply.github.com>
2017-10-10 14:49:05 +1100

    Fix SCons check and add Git check

--- windows/check-dependencies.bat
+++ windows/check-dependencies.bat
@@ -2,7 +2,7 @@
 
 CALL ".\global_variables.bat"
 
-echo =======================================
+echo ==============================================================================
 IF NOT DEFINED VC_PATH (
     echo Visual C++ vcvarsall.bat not found
     echo Please install Visual C++ from here: https://www.visualstudio.com/vs/community/
@@ -11,7 +11,15 @@ IF NOT DEFINED VC_PATH (
     echo Visual C++ vcvarsall.bat was found successfully
 )
 
-echo =======================================
+echo ==============================================================================
+git --version >nul 2>&1 && (
+    echo Git was found successfully
+) || (
+    echo Git not found
+    echo Please install Git from here: https://git-scm.com/download/win
+)
+
+echo ==============================================================================
 python --version >nul 2>&1 && (
     echo Python was found successfully
 ) || (
@@ -21,13 +29,18 @@ python --version >nul 2>&1 && (
     echo NOTE 2: Using the 32-bits installer is recommended
 )
 
-echo =======================================
+echo ==============================================================================
 scons --version >nul 2>&1 && (
     echo SCons was found successfully
+
+    REM This needs to be inside the parentheses because for some reason
+    REM scons breaks the flow and nothing after the parentheses gets called
+    echo ==============================================================================
+    PAUSE
 ) || (
     echo SCons not found
     echo Please install SCons from here: http://www.scons.org/
-)
 
-echo =======================================
-pause
+    echo ==============================================================================
+    PAUSE
+)
