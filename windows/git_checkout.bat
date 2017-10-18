04642430a6d96585caee1ffeac2a8cc17e5a1867
Scott Beca <scott.beca@gmail.com>
2017-10-18 23:16:39 +1100

    Add Windows Git checkout script

---
+++ windows/git_checkout.bat
@@ -0,0 +1,26 @@
+@ECHO off
+
+CALL ".\global_variables.bat"
+
+IF NOT EXIST "%GIT_PARENT_FOLDER%" mkdir "%GIT_PARENT_FOLDER%"
+cd "%GIT_PARENT_FOLDER%"
+
+CALL :DoGitStuff "godot"
+CALL :DoGitStuff "godot-docs"
+CALL :DoGitStuff "godot-demo-projects"
+
+PAUSE
+EXIT /B 0
+
+
+:DoGitStuff
+SET URL=https://github.com/%GIT_USERNAME%/%~1
+IF EXIST "%GIT_PARENT_FOLDER%\%~1" (
+    ECHO === %~1 folder already exists so skipping checkout ===
+) ELSE (
+    git clone %URL%.git
+    cd ./%~1
+    git remote add upstream https://github.com/godotengine/%~1.git
+    cd ..
+)
+EXIT /B 0
--- windows/global_variables.bat
+++ windows/global_variables.bat
@@ -1,21 +1,21 @@
-@echo off
+@ECHO off
 
-SET GIT_PARENT_FOLDER="%USERPROFILE%\Documents\GitHub"
-SET GIT_USERNAME="sbeca"
+SET GIT_PARENT_FOLDER=%USERPROFILE%\Documents\GitHub
+SET GIT_USERNAME=sbeca
 
-SET TEMPLATE_FOLDER="%USERPROFILE%\AppData\Roaming\Godot\templates\3.0-alpha"
-SET EDITOR_BUILD_PATH="%GIT_PARENT_FOLDER%\Godot-3.0-alpha.exe"
+SET TEMPLATE_FOLDER=%USERPROFILE%\AppData\Roaming\Godot\templates\3.0-alpha
+SET EDITOR_BUILD_PATH=%GIT_PARENT_FOLDER%\Godot-3.0-alpha.exe
 SET CPU_CORE_COUNT=%NUMBER_OF_PROCESSORS%
 
-IF exist "C:\Program Files (x86)\Microsoft Visual Studio 15.0\VC\vcvarsall.bat" (
+IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio 15.0\VC\vcvarsall.bat" (
     SET VC_PATH="C:\Program Files (x86)\Microsoft Visual Studio 15.0\VC\vcvarsall.bat"
 )
-IF NOT DEFINED VC_PATH IF exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
+IF NOT DEFINED VC_PATH IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
     SET VC_PATH="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
 )
-IF NOT DEFINED VC_PATH IF exist "C:\Program Files (x86)\Microsoft Visual Studio 13.0\VC\vcvarsall.bat" (
+IF NOT DEFINED VC_PATH IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio 13.0\VC\vcvarsall.bat" (
     SET VC_PATH="C:\Program Files (x86)\Microsoft Visual Studio 13.0\VC\vcvarsall.bat"
 )
-IF NOT DEFINED VC_PATH IF exist "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" (
+IF NOT DEFINED VC_PATH IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" (
     SET VC_PATH="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
 )
